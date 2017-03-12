f18a8bacebd6a2c42505287ebb3c92605e6efe05
jonhue <jonas.huebotter@gmx.de>
2017-03-12 21:41:14 +0100

    Override image_tag Helper

--- lib/generators/templates/amphtml.rb
+++ lib/generators/templates/amphtml.rb
@@ -1,1 +1,19 @@
 require 'amphtml'
+
+module ActionView::Helpers::AssetTagHelper
+
+    def image_tag(source, options = {})
+        options = options.symbolize_keys
+        check_for_image_tag_errors(options)
+
+        src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+
+        unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
+            options[:alt] = options.fetch(:alt) { image_alt(src) }
+        end
+
+        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+        tag("amp-img", options)
+    end
+
+end
