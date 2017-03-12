f7fdf09063c41842c4b94fd104c88f8135d41e20
jonhue <jonas.huebotter@gmx.de>
2017-03-12 23:03:28 +0100

    Fix #1 - Couldn't find AssetTagHelper, expected it to be defined in helpers/asset_tag_helper.rb

--- lib/generators/templates/helpers/asset_tag_helper.rb
+++ lib/generators/templates/helpers/asset_tag_helper.rb
@@ -1,66 +1,88 @@
-module ActionView::Helpers::AssetTagHelper
+require "active_support/core_ext/array/extract_options"
+require "active_support/core_ext/hash/keys"
+require "action_view/helpers/asset_url_helper"
+require "action_view/helpers/tag_helper"
 
-    def image_tag(source, options = {})
-        options = options.symbolize_keys
-        check_for_image_tag_errors(options)
+module ActionView
+  # = Action View Asset Tag Helpers
+  module Helpers #:nodoc:
+    # This module provides methods for generating HTML that links views to assets such
+    # as images, JavaScripts, stylesheets, and feeds. These methods do not verify
+    # the assets exist before linking to them:
+    #
+    #   image_tag("rails.png")
+    #   # => <img alt="Rails" src="/assets/rails.png" />
+    #   stylesheet_link_tag("application")
+    #   # => <link href="/assets/application.css?body=1" media="screen" rel="stylesheet" />
+    module AssetTagHelper
+      extend ActiveSupport::Concern
 
-        src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+      include AssetUrlHelper
+      include TagHelper
 
-        unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
-            options[:alt] = options.fetch(:alt) { image_alt(src) }
+            def image_tag(source, options = {})
+                options = options.symbolize_keys
+                check_for_image_tag_errors(options)
+
+                src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+
+                unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
+                    options[:alt] = options.fetch(:alt) { image_alt(src) }
+                end
+
+                options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+                tag("amp-img", options)
+            end
+
+            def ad_tag(options = {})
+                options = options.symbolize_keys
+                check_for_ad_tag_errors(options)
+
+                options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+                tag("amp-ad", options)
+            end
+
+            def video_tag(source, options = {})
+                options = options.symbolize_keys
+                check_for_video_tag_errors(options)
+
+                src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+
+                unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
+                    options[:alt] = options.fetch(:alt) { image_alt(src) }
+                end
+
+                options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+                tag("amp-video", options)
+            end
+
+            def iframe_tag(source, options = {})
+                options = options.symbolize_keys
+                check_for_iframe_tag_errors(options)
+
+                src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+
+                unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
+                    options[:alt] = options.fetch(:alt) { image_alt(src) }
+                end
+
+                options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
+                tag("amp-iframe", options)
+            end
+
+            def audio_tag(source, options = {})
+                options = options.symbolize_keys
+                check_for_audio_tag_errors(options)
+
+                src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+
+                unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
+                    options[:alt] = options.fetch(:alt) { image_alt(src) }
+                end
+
+                tag("amp-audio", options)
+            end
+
         end
-
-        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
-        tag("amp-img", options)
     end
-
-    def ad_tag(options = {})
-        options = options.symbolize_keys
-        check_for_ad_tag_errors(options)
-
-        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
-        tag("amp-ad", options)
-    end
-
-    def video_tag(source, options = {})
-        options = options.symbolize_keys
-        check_for_video_tag_errors(options)
-
-        src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
-
-        unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
-            options[:alt] = options.fetch(:alt) { image_alt(src) }
-        end
-
-        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
-        tag("amp-video", options)
-    end
-
-    def iframe_tag(source, options = {})
-        options = options.symbolize_keys
-        check_for_iframe_tag_errors(options)
-
-        src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
-
-        unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
-            options[:alt] = options.fetch(:alt) { image_alt(src) }
-        end
-
-        options[:width], options[:height] = extract_dimensions(options.delete(:size)) if options[:size]
-        tag("amp-iframe", options)
-    end
-
-    def audio_tag(source, options = {})
-        options = options.symbolize_keys
-        check_for_audio_tag_errors(options)
-
-        src = options[:src] = path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
-
-        unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
-            options[:alt] = options.fetch(:alt) { image_alt(src) }
-        end
-
-        tag("amp-audio", options)
-    end
-
 end
