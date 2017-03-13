d0916f1b3831ccee6c09102b50de4b2f1db79f66
jonhue <jonas.huebotter@gmx.de>
2017-03-13 01:43:25 +0100

    Update validate Method

--- lib/amphtml.rb
+++ lib/amphtml.rb
@@ -2,11 +2,11 @@ require 'rails'
 
 module Amphtml
 
-    def self.debug(uri)
+    def self.validate(uri)
         require 'launchy'
 
         Launchy.open( uri + "#development=1", :debug ) do |exception|
-            puts "Attempted to open #{uri} and failed because #{exception}"
+            puts "Attempted to open #{uri} and failed because of: #{exception}"
         end
 
         puts "Open the Developer Console and check for validation errors."
