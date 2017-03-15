41dc1731bc96d49661d106ec535d7bca1a25b746
jonhue <jonas.huebotter@gmx.de>
2017-03-15 22:48:26 +0100

    Move validate Method

--- lib/amphtml.rb
+++ lib/amphtml.rb
@@ -18,45 +18,4 @@ module Amphtml
         puts IO.read("lib/amphtml/help/README")
     end
 
-    def self.validate(uri)
-
-        require 'highline'
-        cli = HighLine.new
-        answer = cli.ask "Is your rails server running? [y/n]"
-        unless answer == "y"
-            warn "Please start your rails server first."
-            exit
-        end
-
-        require 'launchy'
-
-        if uri.present? && uri != "root"
-            Launchy.open( uri + "#development=1", :debug ) do |exception|
-                warn "Attempted to open #{uri} and failed because of: #{exception}"
-                exit
-            end
-        else
-            if Rails.env.development?
-                Launchy.open( "localhost:3000" + "#development=1", :debug ) do |exception|
-                    warn "Attempted to open #{uri} and failed because of: #{exception}"
-                    exit
-                end
-            else
-                warn "If you are not in the development environment, you need to specify the exact uri for the site you want to validate."
-                exit
-            end
-        end
-
-        puts "=> Open the Developer Console and check for validation errors."
-
-        exit
-
-    end
-    def self.validate
-        puts 'Please pass the URI you attempt to validate as a parameter or pass "root" to validate your application root.'
-        puts 'Example: Amphtml.validate("https://slooob.com")'
-        puts '**Note:** "root" parameter is only available in development environment!'
-        exit
-    end
-
 end
