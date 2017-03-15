dd8cd76c3b197f42d3a0f9c44b7a233990f9393d
jonhue <jonas.huebotter@gmx.de>
2017-03-15 20:22:01 +0100

    Add link Helpers

--- lib/generators/templates/helpers/amp/amp_tag_helper.rb
+++ lib/generators/templates/helpers/amp/amp_tag_helper.rb
@@ -6,12 +6,26 @@ module ApplicationHelper
 
     def amp_placeholder(name, options: {})
         options[:placeholder] = true
-        content_tag(name, nil, options)
+        content_tag(name, options)
     end
 
     def amp_fallback(name, options: {})
         options[:fallback] = true
-        content_tag(name, nil, options)
+        content_tag(name, options)
+    end
+
+    def amp_link(href)
+        options = {}
+        options[:href] = href
+        options[:rel] = "amphtml"
+        content_tag("link", options)
+    end
+
+    def canonical_link(href)
+        options = {}
+        options[:href] = href
+        options[:rel] = "canonical"
+        content_tag("link", options)
     end
 
 end
