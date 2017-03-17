b5dd0e1e47784c528e433ec81007dd8763c266bc
jonhue <jonas.huebotter@gmx.de>
2017-03-17 16:01:29 +0100

    Fix Format Option in Generators

--- lib/amphtml/components/install.rb
+++ lib/amphtml/components/install.rb
@@ -3,7 +3,7 @@ module Amphtml
         class Install
 
             def self.bundle
-                success = system "rails g amphtml:components#{ ' -f' + Amphtml.raw_format if Amphtml.raw_format }#{ ' --analytics' if Amphtml.components && Amphtml.components.include?('analytics') }#{ ' --iframe' if Amphtml.components && Amphtml.components.include?('iframe') }#{' --notifications' if Amphtml.components && Amphtml.components.include?('notifications') }#{ ' --anim' if Amphtml.components && Amphtml.components.include?('anim') }#{ ' --ad' if Amphtml.components && Amphtml.components.include?('ad') }#{ ' --social' if Amphtml.components && Amphtml.components.include?('social') }"
+                success = system "rails g amphtml:components#{ ' -f ' + Amphtml.raw_format if Amphtml.raw_format }#{ ' --analytics' if Amphtml.components && Amphtml.components.include?('analytics') }#{ ' --iframe' if Amphtml.components && Amphtml.components.include?('iframe') }#{' --notifications' if Amphtml.components && Amphtml.components.include?('notifications') }#{ ' --anim' if Amphtml.components && Amphtml.components.include?('anim') }#{ ' --ad' if Amphtml.components && Amphtml.components.include?('ad') }#{ ' --social' if Amphtml.components && Amphtml.components.include?('social') }"
                 if success
                     puts IO.read("templates/README.md")
                 else
