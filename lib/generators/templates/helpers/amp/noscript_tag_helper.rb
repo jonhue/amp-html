0a6b2022e04f137e3ce335d7877a47cdf7d6f9d6
jonhue <jonas.huebotter@gmx.de>
2017-03-19 14:05:30 +0100

    Fix noscript_tag Helper

--- lib/generators/templates/helpers/amp/noscript_tag_helper.rb
+++ lib/generators/templates/helpers/amp/noscript_tag_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
     module Amp::NoscriptTagHelper
 
-        def noscript_tag
-            content_tag("noscript")
+        def noscript_tag(&block)
+            if block_given?
+                content_tag("noscript", capture(&block))
+            else
+                content_tag("noscript", nil)
+            end
         end
 
     end
